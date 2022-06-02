1baa7af53803a95038d945c57ad71d1b710b761f
Adriaan van Schalkwyk <adriaan@glucode.com>
2022-06-02 11:07:19 +0200

    readd

---
+++ Example/Pods/Target Support Files/Pods-CircleProgressView_Tests/Pods-CircleProgressView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CircleProgressView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CircleProgressView_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-CircleProgressView_Tests/Pods-CircleProgressView_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CircleProgressView_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CircleProgressView_TestsVersionString[];
+
